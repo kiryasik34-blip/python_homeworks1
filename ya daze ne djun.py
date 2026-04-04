d211b3807aa4cec562ca105faa52bb66c2d76ec2
kirill349972 <kirill349972@mail.ru>
2026-04-04 16:29:51 +0500

    TEST2

---
+++ ya daze ne djun.py
@@ -0,0 +1,84 @@
+import random
+
+# list_1 = [1,4,2,5,5]
+# x= sum(list_1)
+# print(x)
+##################
+# lst = [1, 2, 3, 4, 5, 6]
+# for x in lst:
+#     if x % 2 == 0:
+#         print(x)
+#     else:
+#         print("hui")
+###################
+# lst = [1, 2, 3, 4, 5, 6,3,4,5,888,9874,2121]
+# x = max(lst)
+# y = min(lst)
+#
+# if x > y:
+#     print("haha")
+# elif y>x:
+#     print("nihya")
+# else:
+#     print("kal")
+###################
+# lst = [1, 2, 3, 4, 5, 6,3,4,5,888,9874,2121]
+# print(lst[::-1])
+###################
+# n = int(input())
+# total = 0
+#
+# for i in range(1, n+1):
+#     total +=i
+# print(total)
+######################
+# n = int(input())
+# for i in range(1, 900):
+#     print(f"rez={i * n}")
+#######################
+# n = 1
+# i = random.choice(range(1,40))
+# x = n * i
+# print("hui" if x > 10 else f"penis")
+########################
+# for i in range(1, 100):
+#     if i % 3 == 0:
+#         print(i)
+###########################
+
+# d = {'a': 10, 'b': 20, 'c': 30}
+#
+# for k, v in d.items():
+#     if v == 20:
+#         print(k)
+#########################
+
+# d1 = {'a': 1}
+# d2 = {'b': 2, 'c': 3}
+#
+# d1.update(d2)
+# print(d1)
+########################
+
+# lst = [1, 2, 2, 3, 4, 4]
+#
+# result = []
+# for x in lst:
+#     if lst.count(x) == 1:
+#         result.append(x)
+#
+# print(result)
+#########################
+# lst = [1,1,2,2,2,3]
+# d = {}
+#
+# for x in lst:
+#     d[x] = d.get(x, 0) + 1
+#
+# print(d)
+#######################
+# def add(a, b):
+#     return a+b
+# print(add(1,4))
+########################
+
